f57abaf5df5b13807d0cab32d63df25b43737592
Kauan Lazzarin <kauanlazzarin@gmail.com>
2021-06-18 16:31:37 -0400

    html page maded

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const server = express();
 const port = 3003;
 
 server.engine('html', require('ejs').renderFile);
+server.use(session({secret:'jasbckasjdaeljc'})); 
 server.set('view engine', 'html');
 server.use('/public', express.static(path.join(__dirname, 'public')));
 server.set('views', path.join(__dirname, 'views'));
+
+server.get('/', (req, res) => {
+    res.render('index.html')
+})
 
 server.listen(port, () => console.log('executando servidor'));
